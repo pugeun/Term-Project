ab9ec2fe62062d520f6b726845c5eb5a028f7fa2
Soohyuk Choi <144300704+pugeun@users.noreply.github.com>
2023-12-15 19:25:10 +0900

    Update code.py

--- code.py
+++ code.py
@@ -2,19 +2,6 @@ import cv2
 import numpy as np
 
 def find_contours(frame):
-    # 손의 윤곽을 찾기 위한 전처리
-    blurred = cv2.GaussianBlur(frame, (5, 5), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
-    lower_skin = np.array([0, 20, 70], dtype=np.uint8)
-    upper_skin = np.array([20, 255, 255], dtype=np.uint8)
-
-    mask = cv2.inRange(hsv, lower_skin, upper_skin)
-    _, thresh = cv2.threshold(mask, 25, 255, cv2.THRESH_BINARY)
-
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    return contours
 
 def find_hand_contour(frame):
     contours = find_contours(frame)
